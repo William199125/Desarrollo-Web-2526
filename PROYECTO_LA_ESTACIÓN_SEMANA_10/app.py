7f8ca5185ae5c71431df7b5103522d2503849af1
William199125 <wp.zapatai@uea.edu.ec>
2026-02-18 21:19:14 -0500

    Semana 10: Finalización de rutas y herencia de plantillas

--- PROYECTO_LA_ESTACIÓN_SEMANA_10/app.py
+++ PROYECTO_LA_ESTACIÓN_SEMANA_10/app.py
@@ -2,15 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# --- AQUÍ VAN LAS RUTAS ---
+
 @app.route('/')
 def index():
-    # Renderizamos la plantilla index.html en lugar de devolver texto plano
     return render_template('index.html')
 
-# Agrega una ruta extra como pidió el ingeniero (ejemplo: productos)
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 @app.route('/productos')
 def productos():
-    return render_template('index.html') # Luego puedes crear productos.html
+    return render_template('productos.html')
+
+# --------------------------
 
 if __name__ == '__main__':
     app.run(debug=True) 
